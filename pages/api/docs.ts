528f1e7334a5f02e79b0a1dcbe3033ef824cef32
Paul Millar <paul.millar@gmail.com>
2023-06-04 23:34:42 -0500

    added debug

--- pages/api/docs.ts
+++ pages/api/docs.ts
@@ -36,10 +36,10 @@ const handler = async (req: Request): Promise<Response> => {
 
   // OpenAI recommends replacing newlines with spaces for best results
   const input = query.replace(/\n/g, " ");
-  // console.log("input: ", input);
+  console.log("input: ", input);
 
   const apiKey = process.env.OPENAI_API_KEY;
-  
+
   const apiURL = process.env.OPENAI_PROXY == "" ? "https://api.openai.com" : process.env.OPENAI_PROXY;
 
   const embeddingResponse = await fetch(
@@ -101,36 +101,36 @@ const handler = async (req: Request): Promise<Response> => {
   const systemContent = `You are a helpful assistant. When given CONTEXT you answer questions using only that information,
   and you always format your output in markdown. You include code snippets if relevant. If you are unsure and the answer
   is not explicitly written in the CONTEXT provided, you say
-  "Sorry, I don't know how to help with that."  If the CONTEXT includes 
-  source URLs include them under a SOURCES heading at the end of your response. Always include all of the relevant source urls 
+  "Sorry, I don't know how to help with that."  If the CONTEXT includes
+  source URLs include them under a SOURCES heading at the end of your response. Always include all of the relevant source urls
   from the CONTEXT, but never list a URL more than once (ignore trailing forward slashes when comparing for uniqueness). Never include URLs that are not in the CONTEXT sections. Never make up URLs`;
 
   const userContent = `CONTEXT:
   Next.js is a React framework for creating production-ready web applications. It provides a variety of methods for fetching data, a built-in router, and a Next.js Compiler for transforming and minifying JavaScript code. It also includes a built-in Image Component and Automatic Image Optimization for resizing, optimizing, and serving images in modern formats.
   SOURCE: nextjs.org/docs/faq
-  
-  QUESTION: 
-  what is nextjs?    
+
+  QUESTION:
+  what is nextjs?
   `;
 
   const assistantContent = `Next.js is a framework for building production-ready web applications using React. It offers various data fetching options, comes equipped with an integrated router, and features a Next.js compiler for transforming and minifying JavaScript. Additionally, it has an inbuilt Image Component and Automatic Image Optimization that helps resize, optimize, and deliver images in modern formats.
-  
+
   \`\`\`js
   function HomePage() {
     return <div>Welcome to Next.js!</div>
   }
-  
+
   export default HomePage
   \`\`\`
-  
+
   SOURCES:
   https://nextjs.org/docs/faq`;
 
   const userMessage = `CONTEXT:
   ${contextText}
-  
-  USER QUESTION: 
-  ${query}  
+
+  USER QUESTION:
+  ${query}
   `;
 
   const messages = [
